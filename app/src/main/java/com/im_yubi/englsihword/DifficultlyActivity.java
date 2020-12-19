356a47da289efde8855a946be54aa63029c3fc3d
ImUB <dladbql1234@gmail.com>
2020-12-19 21:41:44 +0900

    English Voca App

---
+++ app/src/main/java/com/im_yubi/englsihword/DifficultlyActivity.java
@@ -0,0 +1,12 @@
+package com.im_yubi.englsihword;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DifficultlyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficultly);
+    }
+}
---
+++ app/src/main/java/com/im_yubi/englsihword/MainActivity.java
@@ -0,0 +1,27 @@
+package com.im_yubi.englsihword;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int sub = 1001;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DifficultlyActivity.class);
+                startActivityForResult(intent, sub);
+            }
+        });
+    }
+}
